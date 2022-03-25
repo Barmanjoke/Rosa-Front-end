79e26630d30c046f3dcf45656e691a1c7e9b2632
E-gy <balakhelisey@gmail.com>
2022-03-25 06:18:21 +0100

    #screwit

--- src/rq.js
+++ src/rq.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3060/backend-central"
+const baseURL = "http://rosa-back-central.westeurope.azurecontainer.io:3060/backend-central"
 
 export async function login(email, password){
     const resp = await axios.post(`${baseURL}/user/login`, {email, password});
